805790d04a0c3344afb47e942d978af1a0195b85
Brandon Banks <b93banks@gmail.com>
2017-09-30 23:30:22 -0400

    add pytest and helper methods

---
+++ src/realtor/utils/helpers.py
@@ -0,0 +1,50 @@
+def get_price(house_item):
+    price_string = house_item.find('span', {'class': 'data-price-display'}).text
+    _remove_chars = ['$', ',']
+    for char in _remove_chars:
+        price_string = price_string.replace(char, '')
+    return float(price_string)
+
+
+def get_num_bedrooms(house_item):
+    info = house_item.find('li', {'data-label': 'property-meta-beds'})
+    if info is None:
+        return None
+    return float(info.span.text)
+
+
+def get_num_bathrooms(house_item):
+    info = house_item.find('li', {'data-label': 'property-meta-baths'})
+    if info is None:
+        return None
+    if info.span.text[-1] == '+':
+        return float(info.span.text[:-1]) + .5
+    return float(info.span.text)
+
+
+def get_sqft(house_item):
+    info = house_item.find('li', {'data-label': 'property-meta-sqft'})
+    if info is None:
+        return None
+    return float(info.span.text.replace(',', ''))
+
+
+def get_lot_size(house_item):
+    info = house_item.find('li', {'data-label': 'property-meta-lotsize'})
+    if info is None:
+        return None
+    return float(info.span.text.replace(',', ''))
+
+
+def get_zip_code(house_item):
+    info = house_item.find('span', {'class': 'listing-postal'})
+    if info is None:
+        return None
+    return int(info.text)
+
+
+def get_address(house_item):
+    info = house_item.find('span', {'class': 'listing-street-address'})
+    if info is None:
+        return None
+    return info.text
