235d0861ee00f7f294c76c525e05abad61cbec71
Brandon Banks <b93banks@gmail.com>
2017-10-04 23:57:01 -0400

    remove unused code

--- src/realtor/get_data_scrapy.py
+++ src/realtor/get_data_scrapy.py
@@ -28,12 +28,6 @@ class RealtorScraper(scrapy.Spider):
                 'num_baths': num_baths,
             })
         print(homes)
-        # for quote in response.css("div.quote"):
-        #     yield {
-        #         'text': quote.css("span.text::text").extract_first(),
-        #         'author': quote.css("small.author::text").extract_first(),
-        #         'tags': quote.css("div.tags > a.tag::text").extract()
-        #     }
 
         # next_page_url = response.css('.next .next::attr(href)').extract_first()
         # if next_page_url is not None:
